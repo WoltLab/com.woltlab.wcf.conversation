350145ef607711e849fa196eda5f211b3669395a
Matthias Schmidt <gravatronics@live.com>
2013-04-14 13:35:50 +0200

    Removes obsolete line because of 309274fb

--- files/js/WCF.Conversation.js
+++ files/js/WCF.Conversation.js
@@ -1128,7 +1128,7 @@ WCF.Conversation.Label.Manager = Class.extend({
 	 * @param	object		data
 	 */
 	_insertLabel: function(data) {
-		var $listItem = $('<li><a href="' + this._link + '&labelID=' + data.returnValues.labelID + '"><span class="badge label' + (data.returnValues.cssClassName ? ' ' + data.returnValues.cssClassName : '') + '">' + WCF.String.escapeHTML(data.returnValues.label) + '</span></a></li>');
+		var $listItem = $('<li><a href="' + this._link + '&labelID=' + data.returnValues.labelID + '"><span class="badge label' + (data.returnValues.cssClassName ? ' ' + data.returnValues.cssClassName : '') + '">' + data.returnValues.label + '</span></a></li>');
 		$listItem.find('a > span').data('labelID', data.returnValues.labelID).data('cssClassName', data.returnValues.cssClassName);
 		
 		var $divider = this._labels.find('.dropdownDivider:eq(0)').show();
