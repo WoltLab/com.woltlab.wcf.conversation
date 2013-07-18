50ce0bfc7719bc64a9a8d0fee3561f089326fc8b
Alexander Ebert <ebert@woltlab.com>
2013-07-18 15:45:39 +0200

    Updated SQL

--- update_b3.sql
+++ update_b3.sql
@@ -1,2 +1,5 @@
 /* a9bfc5b */
 ALTER TABLE wcf1_conversation_to_user ADD username VARCHAR(255) NOT NULL DEFAULT '';
+
+/* 7c5aa43 */
+ALTER TABLE wcf1_conversation_label CHANGE cssClassName cssClassName VARCHAR(255) NOT NULL DEFAULT '';
