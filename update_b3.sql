7d449a221cfd08b94dc5a38842a16b3bd1fbe9db
Alexander Ebert <ebert@woltlab.com>
2013-07-30 17:36:37 +0200

    Preparing update 2.0.0 Beta 4

--- update_b3.sql
+++
@@ -1,5 +0,0 @@
-/* a9bfc5b */
-ALTER TABLE wcf1_conversation_to_user ADD username VARCHAR(255) NOT NULL DEFAULT '';
-
-/* 7c5aa43 */
-ALTER TABLE wcf1_conversation_label CHANGE cssClassName cssClassName VARCHAR(255) NOT NULL DEFAULT '';
