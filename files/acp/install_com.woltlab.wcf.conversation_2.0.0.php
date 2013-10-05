22f922c50093eb8a6e474fe6b0c56429b177f925
Matthias Schmidt <gravatronics@live.com>
2013-10-05 17:27:31 +0200

    Fixes installation error if user groups 5/6 don't exists

---
+++ files/acp/install_com.woltlab.wcf.conversation_2.0.0.php
@@ -0,0 +1,31 @@
+<?php
+use wcf\data\user\group\UserGroup;
+use wcf\system\WCF;
+
+/**
+ * @author	Matthias Schmidt
+ * @copyright	2001-2013 WoltLab GmbH
+ * @license	WoltLab Burning Board License <http://www.woltlab.com/products/burning_board/license.php>
+ */
+// set default mod permissions
+$group = new UserGroup(5);
+if ($group->groupID) {
+	$sql = "REPLACE INTO	wcf".WCF_N."_user_group_option_value
+				(groupID, optionID, optionValue)
+		SELECT		5, optionID, 1
+		FROM		wcf".WCF_N."_user_group_option
+		WHERE		optionName LIKE 'mod.conversation.%'";
+	$statement = WCF::getDB()->prepareStatement($sql);
+	$statement->execute();
+}
+
+$group = new UserGroup(6);
+if ($group->groupID) {
+	$sql = "REPLACE INTO	wcf".WCF_N."_user_group_option_value
+				(groupID, optionID, optionValue)
+		SELECT		6, optionID, 1
+		FROM		wcf".WCF_N."_user_group_option
+		WHERE		optionName LIKE 'mod.conversation.%'";
+	$statement = WCF::getDB()->prepareStatement($sql);
+	$statement->execute();
+}
