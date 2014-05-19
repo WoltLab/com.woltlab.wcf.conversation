c05cb94027ce41b66117b3e73ba7ed5c6a13b5c4
Marcel Werk <burntime@woltlab.com>
2014-05-19 19:02:37 +0200

    Added stat handlers

---
+++ files/lib/system/stat/ConversationStatDailyHandler.class.php
@@ -0,0 +1,24 @@
+<?php
+namespace wcf\system\stat;
+
+/**
+ * Stat handler implementation for conversations.
+ *
+ * @author	Marcel Werk
+ * @copyright	2001-2014 WoltLab GmbH
+ * @license	GNU Lesser General Public License <http://opensource.org/licenses/lgpl-license.php>
+ * @package	com.woltlab.wcf.conversation
+ * @subpackage	system.stat
+ * @category	Community Framework
+ */
+class ConversationStatDailyHandler extends AbstractStatDailyHandler {
+	/**
+	 * @see \wcf\system\stat\IStatDailyHandler::getData()
+	 */
+	public function getData($date) {
+		return array(
+			'counter' => $this->getCounter($date, 'wcf'.WCF_N.'_conversation', 'time'),
+			'total' => $this->getTotal($date, 'wcf'.WCF_N.'_conversation', 'time')
+		);
+	}
+}
