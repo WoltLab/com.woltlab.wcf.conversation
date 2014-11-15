216c64f751eb329034b7083b1ef1ab2c930d0dd4
Alexander Ebert <ebert@woltlab.com>
2014-11-15 14:46:07 +0100

    Fixed my previous commit

--- update3_2.1.0_alpha_1.sql
+++ update3_2.1.0_alpha_1.sql
@@ -1,3 +1,1 @@
-ALTER TABLE wcf1_conversation_message ADD lastEditTime INT(10) NOT NULL DEFAULT 0;
-ALTER TABLE wcf1_conversation_message ADD editCount MEDIUMINT(7) NOT NULL DEFAULT 0;
 ALTER TABLE wcf1_conversation_message ADD hasEmbeddedObjects TINYINT(1) NOT NULL DEFAULT 0;
