811f0c8d6b83a5056f21d055eefdf43da67fa45c
Matthias Schmidt <gravatronics@live.com>
2016-05-11 20:34:04 +0200

    Remove unused import

--- files/lib/page/ConversationListPage.class.php
+++ files/lib/page/ConversationListPage.class.php
@@ -3,7 +3,6 @@ namespace wcf\page;
 use wcf\data\conversation\label\ConversationLabel;
 use wcf\data\conversation\label\ConversationLabelList;
 use wcf\data\conversation\UserConversationList;
-use wcf\system\breadcrumb\Breadcrumb;
 use wcf\system\clipboard\ClipboardHandler;
 use wcf\system\exception\IllegalLinkException;
 use wcf\system\page\PageLocationManager;
