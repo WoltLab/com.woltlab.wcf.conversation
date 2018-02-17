ca7ac0d37da90bf5bc4e57532fd5665a343dafe1
Tim Düsterhus <duesterhus@woltlab.com>
2018-02-17 16:15:07 +0100

    Update @copyright dates

--- files/lib/form/ConversationDraftEditForm.class.php
+++ files/lib/form/ConversationDraftEditForm.class.php
@@ -14,7 +14,7 @@ use wcf\util\HeaderUtil;
  * Allows the editing of conversation drafts.
  *
  * @author	Marcel Werk
- * @copyright	2001-2017 WoltLab GmbH
+ * @copyright	2001-2018 WoltLab GmbH
  * @license	GNU Lesser General Public License <http://opensource.org/licenses/lgpl-license.php>
  * @package	WoltLabSuite\Core\Form
  */
--- files/lib/system/page/handler/DefaultConversationRelatedPageHandler.class.php
+++ files/lib/system/page/handler/DefaultConversationRelatedPageHandler.class.php
@@ -7,7 +7,7 @@ namespace wcf\system\page\handler;
  * Only use this class when you need the online location handling for a board-related page.
  *
  * @author	Matthias Schmidt
- * @copyright	2001-2017 WoltLab GmbH
+ * @copyright	2001-2018 WoltLab GmbH
  * @license	GNU Lesser General Public License <http://opensource.org/licenses/lgpl-license.php>
  * @package	WoltLabSuite\Core\System\Page\Handler
  * @since	3.0
