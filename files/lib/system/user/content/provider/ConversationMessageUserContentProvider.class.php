468909d97d97389501320893d42682620b458c76
Joshua Rüsweg <josh@bastelstu.be>
2018-11-26 12:47:07 +0100

    Add conversation message user content provider See WoltLab/WCF#2716

---
+++ files/lib/system/user/content/provider/ConversationMessageUserContentProvider.class.php
@@ -0,0 +1,21 @@
+<?php
+namespace wcf\system\user\content\provider;
+use wcf\data\conversation\message\ConversationMessage;
+
+/**
+ * User content provider for conversation messages.
+ *
+ * @author	Joshua Ruesweg
+ * @copyright	2001-2018 WoltLab GmbH
+ * @license	GNU Lesser General Public License <http://opensource.org/licenses/lgpl-license.php>
+ * @package	WoltLabSuite\Core\System\User\Content\Provider
+ * @since	3.2
+ */
+class ConversationMessageUserContentProvider extends AbstractDatabaseUserContentProvider {
+	/**
+	 * @inheritdoc
+	 */
+	public static function getDatabaseObjectClass() {
+		return ConversationMessage::class;
+	}
+}
