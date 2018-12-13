d55fda797872b0c3135b34f5abc2b59711d22f11
Alexander Ebert <ebert@woltlab.com>
2018-12-13 15:01:24 +0100

    Pruning mechanism for ip addresses
    See WoltLab/WCF#2656

---
+++ files/lib/system/event/listener/ConversationPruneIpAddressesCronjobListener.class.php
@@ -0,0 +1,22 @@
+<?php
+namespace wcf\system\event\listener;
+use wcf\system\cronjob\PruneIpAddressesCronjob;
+
+/**
+ * Prunes the stored ip addresses.
+ * 
+ * @author	Alexander Ebert
+ * @copyright	2001-2018 WoltLab GmbH
+ * @license	GNU Lesser General Public License <http://opensource.org/licenses/lgpl-license.php>
+ * @package	WoltLabSuite\Core\System\Event\Listener
+ * @since       3.2
+ */
+class ConversationPruneIpAddressesCronjobListener implements IParameterizedEventListener {
+	/**
+	 * @inheritDoc
+	 */
+	public function execute($eventObj, $className, $eventName, array &$parameters) {
+		/** @var PruneIpAddressesCronjob $eventObj */
+		$eventObj->columns['wcf'.WCF_N.'_conversation_message']['ipAddress'] = 'time';
+	}
+}
