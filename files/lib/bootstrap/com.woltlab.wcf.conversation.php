9f175a6d413421931f9b0a692b688cf965f255a6
Tim Düsterhus <duesterhus@woltlab.com>
2022-11-22 14:16:49 +0100

    Add bootstrap script
    see 32f697980b0c763a822419aff5eedce3f0a48e53

---
+++ files/lib/bootstrap/com.woltlab.wcf.conversation.php
@@ -0,0 +1,14 @@
+<?php
+
+use wcf\system\event\EventHandler;
+use wcf\system\worker\event\RebuildWorkerCollecting;
+
+return static function (): void {
+    $eventHandler = EventHandler::getInstance();
+
+    $eventHandler->register(RebuildWorkerCollecting::class, static function (RebuildWorkerCollecting $event) {
+        $event->register(\wcf\system\worker\ConversationMessageRebuildDataWorker::class, -5);
+        $event->register(\wcf\system\worker\ConversationRebuildDataWorker::class, 0);
+        $event->register(\wcf\system\worker\ConversationMessageSearchIndexRebuildDataWorker::class, 300);
+    });
+};
