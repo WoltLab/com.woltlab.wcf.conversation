5ae9bcf0d007db40d2fd760efed6ca180f48ff06
Marcel Werk <burntime@woltlab.com>
2023-11-17 15:29:00 +0100

    Fix broken go to last post link
    The link led to an error message if the conversation did not contain any messages visible for the user.

--- files/lib/page/ConversationPage.class.php
+++ files/lib/page/ConversationPage.class.php
@@ -212,8 +212,7 @@ class ConversationPage extends MultipleLinkPage
         // update last visit time count
         if (
             $this->conversation->isNew()
-            && (
-                $this->objectList->getMaxPostTime() > $this->conversation->lastVisitTime
+            && ($this->objectList->getMaxPostTime() > $this->conversation->lastVisitTime
                 || ($this->conversation->joinedAt && !\count($this->objectList))
             )
         ) {
@@ -383,6 +382,10 @@ class ConversationPage extends MultipleLinkPage
         $statement = WCF::getDB()->prepareStatement($sql, 1);
         $statement->execute($this->objectList->getConditionBuilder()->getParameters());
         $row = $statement->fetchArray();
+        if ($row === false) {
+            return;
+        }
+
         HeaderUtil::redirect(
             LinkHandler::getInstance()->getLink(
                 'Conversation',
