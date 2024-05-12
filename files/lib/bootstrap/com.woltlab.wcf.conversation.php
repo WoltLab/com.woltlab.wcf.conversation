2b79a5865860be04cb59eff6ae862fcc1e12df89
Marcel Werk <burntime@woltlab.com>
2024-05-12 12:31:20 +0200

    Migrate to new event organization

--- files/lib/bootstrap/com.woltlab.wcf.conversation.php
+++ files/lib/bootstrap/com.woltlab.wcf.conversation.php
@@ -1,14 +1,16 @@
 <?php
 
 use wcf\system\event\EventHandler;
-use wcf\system\worker\event\RebuildWorkerCollecting;
 
 return static function (): void {
     $eventHandler = EventHandler::getInstance();
 
-    $eventHandler->register(RebuildWorkerCollecting::class, static function (RebuildWorkerCollecting $event) {
-        $event->register(\wcf\system\worker\ConversationMessageRebuildDataWorker::class, -5);
-        $event->register(\wcf\system\worker\ConversationRebuildDataWorker::class, 0);
-        $event->register(\wcf\system\worker\ConversationMessageSearchIndexRebuildDataWorker::class, 300);
-    });
+    $eventHandler->register(
+        \wcf\event\worker\RebuildWorkerCollecting::class,
+        static function (\wcf\event\worker\RebuildWorkerCollecting $event) {
+            $event->register(\wcf\system\worker\ConversationMessageRebuildDataWorker::class, -5);
+            $event->register(\wcf\system\worker\ConversationRebuildDataWorker::class, 0);
+            $event->register(\wcf\system\worker\ConversationMessageSearchIndexRebuildDataWorker::class, 300);
+        }
+    );
 };
