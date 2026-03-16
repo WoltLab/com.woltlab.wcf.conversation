cf9f3942126eae30c140177d9b90b35dcd48d4b9
Marcel Werk <burntime@woltlab.com>
2026-03-16 12:29:43 +0100

    Add missing check for permissions to delete moderated content
    ref https://www.woltlab.com/community/thread/316974-inhalte-in-der-moderation-lassen-sich-ohne-berechtigung-l%C3%B6schen/

--- files/lib/system/moderation/queue/report/ConversationMessageModerationQueueReportHandler.class.php
+++ files/lib/system/moderation/queue/report/ConversationMessageModerationQueueReportHandler.class.php
@@ -193,6 +193,12 @@ class ConversationMessageModerationQueueReportHandler extends AbstractModeration
         }
     }
 
+    #[\Override]
+    public function canRemoveContent(ModerationQueue $queue)
+    {
+        return WCF::getSession()->getPermission('mod.conversation.canModerateConversation');
+    }
+
     /**
      * @inheritDoc
      */
