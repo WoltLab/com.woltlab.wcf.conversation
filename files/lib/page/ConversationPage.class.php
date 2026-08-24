ec682afb82b0d0bc4ba4344cf370291f3f6b939b
Alexander Ebert <ebert@woltlab.com>
2026-08-24 22:31:15 +0200

    Prevent disclosure of modification log entries past the point a user left the conversation

--- files/lib/page/ConversationPage.class.php
+++ files/lib/page/ConversationPage.class.php
@@ -287,7 +287,7 @@ class ConversationPage extends MultipleLinkPage
                         ORDER BY    time";
                 $statement = WCF::getDB()->prepareStatement($sql, 1);
                 $statement->execute([$this->conversationID, $this->objectList->current()->time]);
-                $endTime = $statement->fetchSingleColumn() - 1;
+                $endTime = \min($endTime, $statement->fetchSingleColumn() - 1);
             }
         }
         $this->objectList->rewind();
